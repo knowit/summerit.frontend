8bc4576b6910ac869f106afd2a7cce09007527f3
silchr <silje.christensen@knowit.no>
2016-08-01 18:33:36 +0200

    initial grid

--- app/components/dashboard/dashboard.component.ts
+++ app/components/dashboard/dashboard.component.ts
@@ -2,12 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { Train } from '../../models/train';
 import { TrainService } from '../../services/train.service';
 import { Router } from '@angular/router';
+import { NgGrid, NgGridItem } from 'angular2-grid';
 
 
 @Component({
   selector: 'my-dashboard',
   templateUrl: 'app/components/dashboard/dashboard.component.html',
-  styleUrls: ['app/components/dashboard/dashboard.component.css']
+  styleUrls: ['app/components/dashboard/dashboard.component.css'],
+  directives: [NgGrid, NgGridItem]
+
 })
 export class DashboardComponent implements OnInit {
   trains: Train[] = [];
